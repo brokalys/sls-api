0d8be5c0d87a1490f10605e1e239d97e4fcaf4ee
Matiss Janis Aboltins <matiss@mja.lv>
2018-12-24 10:23:29 +0200

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,104 @@
+const mysql = require('serverless-mysql')({
+  config: {
+    host     : process.env.DB_HOST,
+    database : process.env.DB_DATABASE,
+    user     : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD,
+  },
+});
+const moment  = require('moment');
+const numbers = require('numbers');
+const inside = require('point-in-polygon');
+
+const geojson = require('../data/riga-geojson.json');
+
+// @todo: should have server-side caching so that DB does not get loads of read requests
+exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const type = 'sell'; // @todo: dynamic
+  const category = 'apartment';
+  const start = moment.utc().startOf('month').toISOString();
+  const end = moment.utc().endOf('month').toISOString();
+
+  const data = await getData(start, end, category, type);
+
+  const lastYearData = await getData(
+    moment.utc().subtract(1, 'year').startOf('month').toISOString(),
+    moment.utc().subtract(1, 'year').endOf('month').toISOString(),
+    category,
+    type,
+  );
+
+  const mappedRegions = data.map((data) => {
+    const lastYear = lastYearData.find(({ name }) => data.name === name);
+
+    const buildObj = (a, b) => ({
+      count: (a.count - b.count) / b.count,
+      min: (a.min - b.min) / b.min,
+      max: (a.max - b.max) / b.max,
+      mean: (a.mean - b.mean) / b.mean,
+      median: (a.median - b.median) / b.median,
+      mode: (a.mode - b.mode) / b.mode,
+      standardDev: (a.standardDev - b.standardDev) / b.standardDev,
+    });
+
+    return {
+      ...data,
+      change: {
+        '1yr': buildObj(data, lastYear),
+      },
+    };
+  });
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(mappedRegions),
+  };
+}
+
+async function getData(start, end, category, type) {
+  const regions = geojson.features.map((feature) => ({
+    name: feature.properties.apkaime,
+    polygons: feature.geometry.coordinates,
+  }));
+
+  const data = await mysql.query({
+    sql: `
+      SELECT price, lat, lng
+      FROM properties
+      WHERE published_at BETWEEN ? AND ?
+      AND category = ?
+      AND type = ?
+      AND lat IS NOT NULL
+      AND lng IS NOT NULL
+      AND location_country = "Latvia"
+      AND price > 1
+    `,
+
+    values: [start, end, category, type],
+  });
+
+  await mysql.end();
+
+  return regions
+    .map((region) => ({
+      ...region,
+      prices: data.filter(({ lat, lng }) =>
+        region.polygons.find((polygon) => inside([lng, lat], polygon))
+      ).map(({ price }) => price),
+    }))
+    .map((region) => ({
+      name: region.name,
+      count: region.prices.length,
+      min: numbers.basic.min(region.prices),
+      max: numbers.basic.max(region.prices),
+      mean: numbers.statistic.mean(region.prices),
+      median: numbers.statistic.median(region.prices),
+      mode: numbers.statistic.mode(region.prices),
+      standardDev: numbers.statistic.standardDev(region.prices),
+    }));
+}
