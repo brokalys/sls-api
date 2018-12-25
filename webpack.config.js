67f60a29a62a6c086bc64dd7ee13b92d541512ed
Matiss Janis Aboltins <matiss@mja.lv>
2018-12-25 14:28:56 +0200

    Add serverless-webpack to fix importing issues

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  mode: 'production',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['env', { targets: { node: '8.10' } }]],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+};
