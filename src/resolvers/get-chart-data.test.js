c9bbf431b1421da08c273d39f942a0f6a8e257e9
Matiss Janis Aboltins <matiss@mja.lv>
2019-01-29 22:40:18 +0200

    test: Mock DB for chart resolver rather than get-regions
    Soon I will be refactoring this resolver not to use get-regions class, but to hit the DB directly,
    so this test change will help with that.

--- src/resolvers/get-chart-data.test.js
+++ src/resolvers/get-chart-data.test.js
@@ -1,17 +1,35 @@
+import db from '../lib/db';
 import getChartData from './get-chart-data';
 
 jest.mock('../lib/cache');
-jest.mock('./get-regions', () => ({
-  getRegionsData() {
-    return [
-      { name: 'Āgenskalns', price_per_sqm: { median: 123, count: 12 } },
-      { name: 'Mežaparks', price_per_sqm: { median: null, count: null } },
-      { name: 'Imanta', price_per_sqm: { median: 100, count: 2 } },
-      { name: 'Jugla', price_per_sqm: { median: 100, count: 10 } },
-      { name: 'Torņakalns', price_per_sqm: { median: 40, count: 5 } },
-    ];
+jest.mock('../lib/db');
+
+db.query.mockImplementation(() => [
+  {
+    price: 110000.0,
+    lat: 56.9366684,
+    lng: 24.0794235,
+    price_per_sqm: 100,
+    area: 120,
+    area_measurement: 'm2',
   },
-}));
+  {
+    price: 170000.0,
+    lat: 56.9366684,
+    lng: 24.0794235,
+    price_per_sqm: 0,
+    area: 1,
+    area_measurement: 'ha',
+  },
+  {
+    price: 70000.0,
+    lat: 56.9366684,
+    lng: 24.0794235,
+    price_per_sqm: 0,
+    area: 120,
+    area_measurement: 'm2',
+  },
+]);
 
 describe('getChartData', () => {
   describe('response', () => {
