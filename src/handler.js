ba25b1c5850872ee735cf8fdc19d95eb77b10323
Matiss Janis Aboltins <matiss@mja.lv>
2020-05-12 18:28:55 +0100

    🐛 disable GraphQL playground due to timeouts

--- src/handler.js
+++ src/handler.js
@@ -1,16 +1,6 @@
 import { ApolloServer } from 'apollo-server-lambda';
 
 import mysql from 'lib/db';
-import confirmPinger from './schema/demo/confirm-pinger.graphql';
-import createPinger from './schema/demo/create-pinger.graphql';
-import unsubscribePinger from './schema/demo/unsubscribe-pinger.graphql';
-import getChartDataQuery from './schema/demo/get-chart-data.graphql';
-import getMapDataQuery from './schema/demo/get-map-data.graphql';
-import getMedianPrice from './schema/demo/get-median-price.graphql';
-import getPingerStats from './schema/demo/get-pinger-stats.graphql';
-import getPropertiesForPinger from './schema/demo/get-properties-for-pinger.graphql';
-import getRegionsQuery from './schema/demo/get-regions.graphql';
-import getTableDataQuery from './schema/demo/get-table-data.graphql';
 import schema from './schema/schema.graphql';
 import { resolvers } from './resolvers';
 
@@ -25,70 +15,7 @@ const headers =
 export const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
-  playground: {
-    tabs: [
-      {
-        name: 'Get Regional Stats',
-        endpoint,
-        query: getRegionsQuery.loc.source.body,
-        headers,
-      },
-      {
-        name: 'Get Chart Data',
-        endpoint,
-        query: getChartDataQuery.loc.source.body,
-        headers,
-      },
-      {
-        name: 'Get Map Data',
-        endpoint,
-        query: getMapDataQuery.loc.source.body,
-        headers,
-      },
-      {
-        name: 'Get Table Data',
-        endpoint,
-        query: getTableDataQuery.loc.source.body,
-        headers,
-      },
-      {
-        name: 'Get stats for PINGER',
-        endpoint,
-        query: getPingerStats.loc.source.body,
-        headers,
-      },
-      {
-        name: 'Create new PINGER',
-        endpoint,
-        query: createPinger.loc.source.body,
-        headers,
-      },
-      {
-        name: 'Unsubscribe PINGER',
-        endpoint,
-        query: unsubscribePinger.loc.source.body,
-        headers,
-      },
-      {
-        name: 'Confirm PINGER',
-        endpoint,
-        query: confirmPinger.loc.source.body,
-        headers,
-      },
-      {
-        name: 'Get Median price for a region & month',
-        endpoint,
-        query: getMedianPrice.loc.source.body,
-        headers,
-      },
-      {
-        name: 'Get properties for pinger',
-        endpoint,
-        query: getPropertiesForPinger.loc.source.body,
-        headers,
-      },
-    ],
-  },
+  playground: false,
   context: ({ event, req }) => {
     const { headers } = event || req || { headers: {} };
     return {
--- src/lib/db.js
+++ src/lib/db.js
@@ -7,27 +7,6 @@ const mysql = serverlessMysql({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
   },
-  onError: () => {
-    console.log('onError');
-  },
-  onClose: () => {
-    console.log('onClose');
-  },
-  onConnect: () => {
-    console.log('onConnect');
-  },
-  onConnectError: () => {
-    console.log('onConnectError');
-  },
-  onKill: () => {
-    console.log('onKill');
-  },
-  onKillError: () => {
-    console.log('onKillError');
-  },
-  onRetry: () => {
-    console.log('onRetry');
-  },
   maxRetries: 5,
 });
 
