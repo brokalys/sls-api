080f778e54555d808725d1e7d04928468aa77f29
Matiss Janis Aboltins <matiss@mja.lv>
2020-06-21 20:51:46 +0100

    :ambulance: set SQS api version

--- src/lib/sqs.js
+++ src/lib/sqs.js
@@ -1,7 +1,9 @@
 import AWS from 'aws-sdk';
 
+AWS.config.update({ region: process.env.AWS_REGION });
+
 const sqs = new AWS.SQS({
-  region: process.env.AWS_REGION,
+  apiVersion: '2012-11-05',
 });
 
 export function sendMessage(message) {
